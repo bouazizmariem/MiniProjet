4f882deaf87b5d545c1bd5ffd0f85d5445db4d58
BenJdidiaMariem <167816873+BenJdidiaMariem@users.noreply.github.com>
2025-01-12 21:27:25 +0100

    Add files via upload

---
+++ user/inscrit.sql
@@ -0,0 +1,9 @@
+
+
+CREATE TABLE `inscrit` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `prenom` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL
+) 
